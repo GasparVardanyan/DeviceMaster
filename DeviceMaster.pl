ab3efada92db4f070f34ce9891f7fa3fcc111c4a
GasparVardanyan <gaspar_pm@proton.me>
2025-12-09 19:10:49 +0400

    proper cli app for jrpc

--- DeviceMaster.pl
+++ DeviceMaster.pl
@@ -5,11 +5,61 @@ use warnings;
 
 use lib '.';
 
-use DeviceMaster::Apps::JRPC;
-use DeviceMaster::DeviceSystem;
+package DeviceMaster::App {
+	use MooseX::App;
+}
 
-my $jrpc = DeviceMaster::Apps::JRPC->new (
-	path => '/tmp/devicemaster.socket'
-);
+package DeviceMaster::App::JRPC {
+	use MooseX::App::Command;
 
-$jrpc->listen;
+	use Fcntl qw( :mode );
+
+	use DeviceMaster::Apps::JRPC;
+	extends 'DeviceMaster::App';
+
+	option 'socket_file' => (
+		is => 'ro',
+		isa => 'Str',
+		documentation => 'the socket file path',
+		default => '/tmp/devicemaster.socket'
+	);
+
+	option group => (
+		is => 'ro',
+		documentation => 'group to own the socket file'
+	);
+
+	has jrpc => (
+		is => 'ro',
+		isa => 'DeviceMaster::Apps::JRPC',
+		default => sub {
+			my $self = shift;
+
+			return DeviceMaster::Apps::JRPC->new (
+				path => $self->socket_file
+			);
+		},
+		lazy => 1
+	);
+
+	sub run {
+		my $self = shift;
+
+		if (-S $self->socket_file) {
+			unlink $self->socket_file;
+		}
+
+		$self->jrpc->server;
+
+		chmod S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP, $self->socket_file;
+
+		if (defined $self->group) {
+			# chgrp group socket_file - the most complicated way possible:
+			chown ((stat ($self->socket_file)) [4], (getgrnam ($self->group)) [2], $self->socket_file);
+		}
+
+		$self->jrpc->listen;
+	}
+}
+
+DeviceMaster::App->new_with_command->run;
